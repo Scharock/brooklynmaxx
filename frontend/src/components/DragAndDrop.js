f73911c1fb908bf84398313476a02ca293292574
Scharock <bloodmaster04@gmx.de>
2020-09-07 16:11:20 +0200

    move saveSelectedFiles to own function and add missing onChange for the input file element and call the new function to save the files

--- frontend/src/components/DragAndDrop.js
+++ frontend/src/components/DragAndDrop.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react'
 
 class DragAndDrop extends Component {
+    labelDefaultText = 'Choose a file';
+    dropRef = React.createRef();
+    dragCounter = 0;
+
     state = {
         drag: false,
-        label: 'Choose a file',
+        label: this.labelDefaultText,
         dropped: false,
         droppedFiles: new FormData()
     }
-
-    dropRef = React.createRef();
-    dragCounter = 0;
 
     handleDrag = (e) => {
         e.preventDefault();
@@ -40,30 +41,39 @@ class DragAndDrop extends Component {
         this.setState({drag: false});
 
         if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-            let fileCount = e.dataTransfer.files.length,
-                files = e.dataTransfer.files,
-                fileList = new FormData();
-
-            if (fileCount > 1) {
-                this.setState({
-                    label: fileCount + ' files selected',
-                    dropped: true
-                } );
-            } else {
-                this.setState({
-                    label: e.dataTransfer.files.item(0).name,
-                    dropped: true
-                });
-            }
-
-            for (var i = 0; i < files.length; i++) {
-                if (!files[i].name) return
-                fileList.append(files[i].name, files[i])
-            }
-
-            this.setState({droppedFiles: fileList});
+            this.saveSelectedFiles(e.dataTransfer.files);
             this.dragCounter = 0;
         }
+    }
+
+    handleChange = (event) => {
+        let files = event.target.files;
+
+        this.saveSelectedFiles(files);
+    }
+
+    saveSelectedFiles = (files) => {
+        let fileCount = files.length,
+            fileList = new FormData();
+
+        if (fileCount > 1) {
+            this.setState({
+                label: fileCount + ' files selected',
+                dropped: true
+            } );
+        } else {
+            this.setState({
+                label: files.item(0).name,
+                dropped: true
+            });
+        }
+
+        for (var i = 0; i < files.length; i++) {
+            if (!files[i].name) return
+            fileList.append(files[i].name, files[i])
+        }
+
+        this.setState({droppedFiles: fileList});
     }
 
     handleSubmit = (event) => {
@@ -75,6 +85,8 @@ class DragAndDrop extends Component {
                 method: 'POST',
                 body: this.state.droppedFiles
             }
+        ).then(
+            this.setState({label: this.labelDefaultText, dropped: false})
         )
     }
 
@@ -99,7 +111,7 @@ class DragAndDrop extends Component {
             <form className={'draganddrop--box' + (this.state.drag ? ' is-dragover' : '')} method={'post'} action={''} encType={'multipart/form-data'} ref={this.dropRef} onSubmit={this.handleSubmit}>
                 <div className={'draganddrop--box__input'}>
                     <img src={'img/upload-solid.svg'} className={'draganddrop--box__icon'} alt={'upload icon'} />
-                    <input type="file" className={'draganddrop--box__file'} name={'files[]'} id={'file'} multiple={''}/>
+                    <input type="file" className={'draganddrop--box__file'} name={'files[]'} id={'file'} multiple={''} onChange={this.handleChange}/>
                     <label htmlFor={'file'} className={'draganddrop--box__label'}>{this.state.label}</label>{this.state.dropped === false && <span className={'draganddrop--box__text'}>or drag it here</span>}
                     <button className={'draganddrop--box__submit'} type={"submit"}>Upload</button>
                 </div>
